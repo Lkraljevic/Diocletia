af43c1747a9693c595816d0f2f2a94f7fa6df6ef
Luka Kraljević <luka.kraljevic90@gmail.com>
2018-05-05 10:44:07 +0200

    gallery

--- web/js/contact.js
+++ web/js/contact.js
@@ -14,9 +14,6 @@ function contact_onSubmit(response) {
     var subject = contactForm["subject"].value;
     var message = contactForm["message"].value;
 
-    console.log(response);
-    console.log(name, email, subject, message);
-
     submitMesage({
         formData: { name,email,subject,message },
         response: response
@@ -35,15 +32,15 @@ function contact_onSubmit(response) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        if(this.responseText.success) {
+        var responseText = JSON.parse(this.responseText);
+        if(responseText.success) {
             var response =  document.getElementById('contact-response');
             if(response) response.classList.add('contact__message--visible');
             contactForm.style.display = 'none';
         }
       }
     };
-    console.log(data);
-
+    
     xhttp.open("POST", "https://diocletia.hr/googlevalidate.php", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(JSON.stringify(data));
